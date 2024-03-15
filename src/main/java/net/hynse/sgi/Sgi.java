a87fc7973d850cd232a3d4bbce5ddad1631a9690
MidnightTale <midnighttale24@gmail.com>
2024-03-16 05:21:04 +0700

    hello

---
+++ src/main/java/net/hynse/sgi/Sgi.java
@@ -0,0 +1,17 @@
+package net.hynse.sgi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Sgi extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
